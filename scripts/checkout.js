29472e744c71ffe4423c4a51b4d5f6aeb45702ce
GaragaNaveenKumar <23a35a0517@pragati.ac.in>
2025-02-25 18:25:08 +0530

    regenerate html when upadate delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,8 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../data/deliveryOptions.js';
 
 
-
+// function to render order summary
+function renderOrderSummary(){
 let cartSummaryHTML='';
 
 
@@ -157,7 +158,10 @@ document.querySelectorAll('.js-delivery-option')
     element.addEventListener('click',()=>{
         const {productId,deliveryOptionId}=element.dataset;
         updateDeliveryOption(productId,deliveryOptionId);
+        renderOrderSummary();
 
     });
 
 });
+}
+renderOrderSummary();
