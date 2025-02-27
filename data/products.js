f9bb7ca06a9cf891621020c59b79f35eb059ed0b
GaragaNaveenKumar <23a35a0517@pragati.ac.in>
2025-02-27 16:18:54 +0530

    load products from backend

--- data/products.js
+++ data/products.js
@@ -66,6 +66,35 @@ function fun(){
 fun();
 fun.call('hello');*/
 
+
+export let products=[];
+export function loadProducts(fun){
+  const xhr=new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    products=JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type==='clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    
+    });
+    console.log('loading products ...');
+
+    fun();
+
+  });
+
+
+
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -731,4 +760,4 @@ export const products = [
   }
   return new Product(productDetails);
 
-});
+});*/
